11ddf30aa5e73126fbbe5ae0bb4d767ce7a0bce2
Anthoni Mathias <anthonimathias@gmail.com>
2021-06-27 12:18:14 +0100

    Start button beard for inner html

--- assets/js/menucart.js
+++ assets/js/menucart.js
@@ -1,3 +1,10 @@
 $("beard-button").click(function () {
   document.getElementById("beard-button").classList.add("btn-success");
+  const gentsCut = document.getElementById("gents-haircut");
+  gentsCut.classList.remove("btn-success");
+  const colorHighlight = document.getElementById("color-button");
+  colorHighlight.classList.remove("btn-success");
+  const homeServices = document.getElementById("homeservices-button");
+  homeServices.classList.remove("btn-success");
+  document.getElementById("shop-thing").innerHTML
 });
