81e4603ef770c4087540e849420703c72183567b
Anthoni Mathias <anthonimathias@gmail.com>
2021-06-27 12:25:25 +0100

    Adde Ready funciton

--- assets/js/menucart.js
+++ assets/js/menucart.js
@@ -6,5 +6,23 @@ $("beard-button").click(function () {
   colorHighlight.classList.remove("btn-success");
   const homeServices = document.getElementById("homeservices-button");
   homeServices.classList.remove("btn-success");
-  document.getElementById("shop-thing").innerHTML
+  document.getElementById("shop-thing").innerHTML = `
+  <div class="shop-item">
+    <span class="shop-item-title">Regular Cut</span>
+    <div class="shop-item-details">
+        <p>
+          This service includes a consultation, followed by machine back & sides and scissors on top.                           
+        </p>
+        <span class="shop-item-price">€18 - €20</span>
+        <button class="btn btn-primary shop-item-button" type="button">
+          Add to cart
+        </button>
+    </div>
+</div> `;
+ready();
 });
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready);
+} else {
+  ready();
+}
