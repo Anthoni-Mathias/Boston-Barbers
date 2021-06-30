03f36488db2631d054aa6646b67534000f014302
Anthoni Mathias <anthonimathias@gmail.com>
2021-06-30 09:35:42 +0100

    Add function ready in menucart

--- assets/js/menucart.js
+++ assets/js/menucart.js
@@ -26,3 +26,22 @@ if (document.readyState == 'loading') {
 } else {
   ready();
 }
+
+function ready() {
+  let removeCartItemButtons = document.getElementsByClassName('btn-danger');
+  for (let i = 0; i < removeCartItemButtons.length; i++) {
+      let button = removeCartItemButtons[i];
+      button.addEventListener('click', removeCartItem);
+  }
+  let quantityInputs = document.getElementsByClassName('cart-quantity-input');
+  for (let i = 0; i < quantityInputs.length; i++) {
+      let input = quantityInputs[i];
+      input.addEventListener('change', quantityChanged);
+  }
+  let addToCartButtons = document.getElementsByClassName('shop-item-button');
+  for (let i = 0; i < addToCartButtons.length; i++) {
+      let button = addToCartButtons[i];
+      button.addEventListener('click', addToCartClicked);
+  }
+  document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked);
+}
