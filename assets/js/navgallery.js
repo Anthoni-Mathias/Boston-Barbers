c00992a833096efb9ac3d4fa6fe0473c7c3f74f3
Anthoni Mathias <anthonimathias@gmail.com>
2021-07-05 09:22:32 +0100

    Fixed bug in path for galerry js

--- assets/js/navgallery.js
+++ assets/js/navgallery.js
@@ -1,33 +1,35 @@
 // Scroll to nav function
-$('a[href^=#services').click(function () {
+$('a[href^="#services"]').click(function () {
   let findPage = document.getElementById("services");
-  findPage = scrollIntoView();
+  findPage.scrollIntoView();
 });
 
-$('a[href^=#gallery').click(function () {
+$('a[href^="#gallery"]').click(function () {
   let findPage = document.getElementById("gallery");
-  findPage = scrollIntoView();
+  findPage.scrollIntoView();
 });
 
-$('a[href^=#book').click(function () {
+$('a[href^="#book"]').click(function () {
   let findPage = document.getElementById("book");
-  findPage = scrollIntoView();
+  findPage.scrollIntoView();
 });
 
-$('a[href^=#contact').click(function () {
+$('a[href^="#contact"]').click(function () {
   let findPage = document.getElementById("contact");
-  findPage = scrollIntoView();
+  findPage.scrollIntoView();
 });
 // Scroll to nav function //
 
 // Nav icon on smaller screens collapse when clicking a link
+// Credit to Marta Szymanska at Mdbootstrap for hamburger collapse code: https://mdbootstrap.com/support/general/auto-close-navbar-when-click-on-link-responsive-mode/
 $('.navbar-collapse a').click(function () {
   $(".navbar-collapse").collapse('hide');
 });
-
 // Nav icon on smaller screens collapse when clicking a link //
 
 // Gallery section
+// Credit to freeCodeCamp for gallery section slideshow: https://www.youtube.com/watch?v=3PHXvlpOkf4&ab_channel=freeCodeCamp.org
+// https://github.com/john-smilga/javascript-basic-projects/tree/master/15-slider
 const slides = document.querySelectorAll(".slide");
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
@@ -61,3 +63,9 @@ function carousel() {
   });
 }
 prevBtn.style.display = "none";
+// Gallery section //
+
+
+
+
+
