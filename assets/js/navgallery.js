87e55c3ab8c1a2e766a0e35d9670b6fc2ecd7cb6
Anthoni Mathias <anthonimathias@gmail.com>
2021-07-05 08:50:43 +0100

    Add more ids to scroll to nav function

--- assets/js/navgallery.js
+++ assets/js/navgallery.js
@@ -3,3 +3,15 @@ $('a[href^=#services').click(function () {
   let findPage = document.getElementById("services");
   findPage = scrollIntoView();
 });
+$('a[href^=#gallery').click(function () {
+  let findPage = document.getElementById("gallery");
+  findPage = scrollIntoView();
+});
+$('a[href^=#book').click(function () {
+  let findPage = document.getElementById("book");
+  findPage = scrollIntoView();
+});
+$('a[href^=#contact').click(function () {
+  let findPage = document.getElementById("contact");
+  findPage = scrollIntoView();
+});
