00619522dea257eb65a0f97fd92160a4d90d18ce
Anthoni Mathias <anthonimathias@gmail.com>
2021-07-05 08:54:09 +0100

    Nav icon on smaller screens collapse when clicking a link

--- assets/js/navgallery.js
+++ assets/js/navgallery.js
@@ -3,15 +3,24 @@ $('a[href^=#services').click(function () {
   let findPage = document.getElementById("services");
   findPage = scrollIntoView();
 });
+
 $('a[href^=#gallery').click(function () {
   let findPage = document.getElementById("gallery");
   findPage = scrollIntoView();
 });
+
 $('a[href^=#book').click(function () {
   let findPage = document.getElementById("book");
   findPage = scrollIntoView();
 });
+
 $('a[href^=#contact').click(function () {
   let findPage = document.getElementById("contact");
   findPage = scrollIntoView();
+});
+// Scroll to nav function //
+
+// Nav icon on smaller screens collapse when clicking a link
+$('.navbar-collapse a').click(function () {
+  $(".navbar-collapse").collapse('hide');
 });
