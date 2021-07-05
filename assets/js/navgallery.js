f4816db1f9ea567446ba998fe0cb10e20d83a37f
Anthoni Mathias <anthonimathias@gmail.com>
2021-07-05 09:09:22 +0100

    Finish the corousel

--- assets/js/navgallery.js
+++ assets/js/navgallery.js
@@ -38,4 +38,26 @@ let counter = 0;
 nextBtn.addEventListener("click", function () {
   counter++;
   carousel();
-})
+});
+
+prevBtn.addEventListener("click", function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  if (counter < slides.length - 1) {
+      nextBtn.style.display = "block";
+  } else {
+      nextBtn.style.display = "none";
+  }
+  if (counter > 0) {
+      prevBtn.style.display = "block";
+  } else {
+      prevBtn.style.display = "none";
+  }
+  slides.forEach(function (slide) {
+      slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
+prevBtn.style.display = "none";
