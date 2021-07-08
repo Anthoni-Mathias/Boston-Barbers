bedabb64d3182db3bc2a031255f0089655f86f64
Anthoni Mathias <63785517+Anthoni-Mathias@users.noreply.github.com>
2021-07-08 11:48:17 +0100

    Finish description to beard services

--- assets/js/menucart.js
+++ assets/js/menucart.js
@@ -74,23 +74,10 @@ $("#highlight-button").click(function () {
   homeservices.classList.remove("btn-success");
   document.getElementById("shop-thing").innerHTML = `
   <div class="shop-item">
-  <span class="shop-item-title">Regular Cut</span>
+  <span class="shop-item-title"> Beard Design </span>
   <div class="shop-item-details">
       <p>
-          This service includes a consultation, followed by machine back & sides and scissors on top.                           
-      </p>
-      <span class="shop-item-price"> €18 </span>
-      <button class="btn btn-primary shop-item-button" type="button">
-          Add to cart
-      </button>
-  </div>
-</div>
-
-<div class="shop-item">
-  <span class="shop-item-title">Skinfade/Bald fade</span>
-  <div class="shop-item-details">
-      <p>
-          Also know as a bald fade or zero fade, the skin fade haircut comes in a variety of forms, including the hight, mid. and low skin fade                       
+          Beard design will trim & moustache to your desidered look. We use cut throat razor to line the beard to ensure the best shape to suit you. Finished with product to suit your beard type.                       
       </p>
       <span class="shop-item-price"> €20 </span>
       <button class="btn btn-primary shop-item-button" type="button">
@@ -100,26 +87,27 @@ $("#highlight-button").click(function () {
 </div>
 
 <div class="shop-item">
-  <span class="shop-item-title">
-      Scissors Precision cut/ Hot towel tonic  finish                     
-  </span>
+  <span class="shop-item-title"> Beard trim </span>
   <div class="shop-item-details">
       <p>
-          A Precision cut, cut and an intensive scalp treatment to stimulate dry and irritable scalp finished off with refreshing hot towel.                        
+            This service will trim & tidy your beard *this service is a regular dry beard trim*(if you wish to have "Beard Design" witch is using the cut throat razor to line around the beard & reshape - please select service "Beard Design)                  
       </p>
-      <span class="shop-item-price"> €23 </span>
+      <span class="shop-item-price"> €10 </span>
       <button class="btn btn-primary shop-item-button" type="button">
           Add to cart
       </button>
   </div>
 </div>
+
 <div class="shop-item">
-  <span class="shop-item-title">All over Machine Buzz cut</span>
+  <span class="shop-item-title">
+      Hot towel Shave                     
+  </span>
   <div class="shop-item-details">
       <p>
-          Machine all over, no scissors used in this service.                         
+        Your face is then prepared with a hot towel treatment, followed by the application of the shaving cream of your choice (and we have a wide range)-, whipped up into a rich lather and applied using a traditional badger hair shaving brush. One of our master barbers will then perform the shave itself.                   
       </p>
-      <span class="shop-item-price">€15</span>
+      <span class="shop-item-price"> €30 </span>
       <button class="btn btn-primary shop-item-button" type="button">
           Add to cart
       </button>
